daf057b38d6b1dbc6846092f8193a3767feb51d4
DTH235618-LamNgocHungCuong <cuong_dth235618@student.agu.edu.vn>
2025-12-01 10:17:10 +0700

    Update QLKS_DA.sql

--- QLKS_DA.sql
+++ QLKS_DA.sql
@@ -1,8 +1,8 @@
-﻿CREATE DATABASE QLKS_DA
+CREATE DATABASE QLKS_DA
 ON
 (
 	NAME = N'QLKS_DA_d',
-	FILENAME = N'C:\QLKS\QLKS_DA_data.mdf',
+	FILENAME = N'D:\QLKS\QLKS_DA_data.mdf',
 	SIZE = 8MB,
 	MAXSIZE = 80MB,
 	FILEGROWTH = 2MB
@@ -10,25 +10,19 @@ ON
 LOG ON 
 (
 	NAME = N'QLKS_DA_lg',
-	FILENAME = N'C:\QLKS\QLKS_DA_log.ldf',
+	FILENAME = N'D:\QLKS\QLKS_DA_log.ldf',
 	SIZE = 8MB,
 	FILEGROWTH = 2MB
-)
-
-
-
+);
 
 USE master;
 GO
 
-
 ALTER DATABASE QLKS_DA SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 GO
 
-
 DROP DATABASE QLKS_DA;
 GO
-
 
 USE QLKS_DA
 
@@ -46,8 +40,8 @@ GO
 CREATE TABLE Phong
 (
     MaPh VARCHAR(5) check(MaPh like'[A-Z][A-Z][0-9][0-9][0-9]') NOT NULL,
-	LoaiPh NVARCHAR(100) NOT NULL UNIQUE,
-    GiaPh NUMERIC(10,3)  DEFAULT 0 check(GiaPh >=0) UNIQUE,
+	LoaiPh NVARCHAR(100) NOT NULL,
+    GiaPh NUMERIC(10,3)  DEFAULT 0 check(GiaPh >=0),
     Tang INT check(Tang >= 0 AND Tang <= 20) NOT NULL, 
 
     PRIMARY KEY (MaPh),
@@ -71,8 +65,6 @@ CREATE TABLE KhachHang
 );
 GO
 
-
-
 CREATE TABLE NhanVien
 (
     MaNV VARCHAR(5) check(MaNV like'[A-Z][0-9][A-Z][0-9][0-9]')  NOT NULL,
@@ -80,7 +72,7 @@ CREATE TABLE NhanVien
     GioiTinhNV nvarchar (10) NOT NULL check (GioiTinhNV = N'Nam' or GioiTinhNV =  N'Nữ'),
     DThoaiNV VARCHAR(10) NOT NULL check (DThoaiNV like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
     DChiNV NVARCHAR(100) NOT NULL,
-    ChucVu NVARCHAR (100) NOT NULL
+    ChucVu NVARCHAR (100) NOT NULL,
     TangTruc INT check(TangTruc >= 0 AND TangTruc <= 20) NOT NULL,
 	MatKhau VARCHAR(50) NOT NULL,
 
@@ -94,14 +86,14 @@ GO
 
 CREATE TABLE ThuePhong
 (
-    MaTP VARCHAR(6) check(MAHD like'[A-Z][A-Z][0-9][0-9][0-9][0-9]') NOT NULL,
-    MaKH VARCHAR(4) check(MAKH like'[A-Z][0-9][0-9][0-9]') NOT NULL,
+    MaTP VARCHAR(6) check(MaTP like'[A-Z][A-Z][0-9][0-9][0-9][0-9]') NOT NULL,
+    MaKH VARCHAR(4) check(MaKH like'[A-Z][0-9][0-9][0-9]') NOT NULL,
     MaNV VARCHAR(5) check(MaNV like'[A-Z][0-9][A-Z][0-9][0-9]')  NOT NULL,
     MaPh VARCHAR(5) check(MAPh like'[A-Z][A-Z][0-9][0-9][0-9]')  NOT NULL,
     NgDen DATE NOT NULL, 
     NgDi DATE NOT NULL,
     SoNgay AS (DATEDIFF (day,NgDen,NgDi)) PERSISTED,
-    ThanhTien NUMERIC(18,3)  DEFAULT 0 check(TongTien >=0) NULL,
+    ThanhTien NUMERIC(18,3)  DEFAULT 0 check(ThanhTien >=0) NULL,
 
     PRIMARY KEY (MaTP),
     FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH),
@@ -115,10 +107,9 @@ CREATE TABLE HoaDon
 (
     MaHD VARCHAR(6) check(MAHD like'[A-Z][A-Z][0-9][0-9][0-9][0-9]') NOT NULL,
     MaKH VARCHAR(4) check(MAKH like'[A-Z][0-9][0-9][0-9]') NOT NULL,
-    MaTP VARCHAR(6) check(MAHD like'[A-Z][A-Z][0-9][0-9][0-9][0-9]') NOT NULL,
+    MaTP VARCHAR(6) check(MaTP like'[A-Z][A-Z][0-9][0-9][0-9][0-9]') NOT NULL,
     MaNV VARCHAR(5) check(MaNV like'[A-Z][0-9][A-Z][0-9][0-9]')  NOT NULL,
     TongTien NUMERIC(18,3)  DEFAULT 0 check(TongTien >=0) NULL,
-
 
     PRIMARY KEY (MaHD),
     FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH),
@@ -139,10 +130,10 @@ Insert into Tang values ('8')
 Insert into Tang values ('9')
 
 
-Insert into Phong values ('GG133',N'Phòng gia đình', '100.000')
-Insert into Phong values ('GI320',N'Giường Đôi', '80.000')
+Insert into Phong values ('GG133',N'Phòng gia đình', '100.000','1')
+Insert into Phong values ('GI320',N'Giường Đôi', '80.000','3')
 Insert into Phong values ('GN510',N'Giường Đơn', '50.000','5')
-Insert into Phong values ('GI920',N'Giường Đôi', '80.000')
+Insert into Phong values ('GI920',N'Giường Đôi', '80.000','9')
 
 
 
@@ -152,14 +143,14 @@ Insert into KhachHang values ('A195',N'Lê Thị Kim', N'Nữ', '0989457330',N'T
 
 
 
-Insert into NhanVien values ('T8Z23', N'Nguyễn Thanh Kim', N'Nữ', '0876235117', N'Tình An Giang, Phường Long Xuyên', N'Nhan viên dọn vệ sinh', '8', 'KL3017')
-Insert into NhanVien values ('T7B52', N'Lê Văn Điền', N'Nam', '0811762357', N'Tình An Giang, Phường Long Xuyên', N'Nhân viên dọn vệ sinh', '7', '93433624')
-Insert into NhanVien values ('A1K59', N'Trần Thị Mai', N'Nữ', '0911237656', N'Tình An Giang, Phường Long Xuyên', N'Tiếp tân', '0', 'KPYFTYTG')
+Insert into NhanVien values ('T8Z23',N'Nguyễn Thanh Kim',N'Nữ','0876235117',N'Tình An Giang, Phường Long Xuyên',N'Nhan viên dọn vệ sinh','8','KL3017')
+Insert into NhanVien values ('T7B52', N'Lê Văn Điền', N'Nam', '0811762357', N'Tình An Giang, Phường Long Xuyên',N'Nhân viên dọn vệ sinh','7','93433624')
+Insert into NhanVien values ('A1K59', N'Trần Thị Mai', N'Nữ', '0911237656', N'Tình An Giang, Phường Long Xuyên',N'Tiếp tân', '0', 'KPYFTYTG')
 
 
 Insert into ThuePhong values ('TT5713', 'A195', 'A1K59', 'GI920', '2025-11-10', '2025-11-20', NULL)
 Insert into ThuePhong values ('TQ7783', 'D308', 'A1K59', 'GN510', '2025-10-20', '2025-11-05', NULL)
-Insert into ThuePhong values ('TT7873', 'D308', 'A1K59', ‘GN510', '2025-12-30', '2026-01-20', NULL)
+Insert into ThuePhong values ('TT7873', 'D308', 'A1K59', 'GN510', '2025-12-30', '2026-01-20', NULL)
 
 
 Insert into HoaDon values ('HT5713', 'A195', 'TT5713', 'A1K59', NULL)
@@ -169,14 +160,9 @@ Insert into HoaDon values ('HT7873', 'D308', 'TT7873', 'A1K59', NULL)
 
 
 SELECT*FROM Tang
-SELECT*FROM LoaiPhong
 SELECT*FROM Phong
 SELECT*FROM KhachHang
-SELECT*FROM ChucVu
 SELECT*FROM NhanVien
-
-
---SELECT*FROM LoaiPhong
 --SELECT*FROM Phong
 SELECT*FROM ThuePhong
 SELECT*FROM HoaDon
@@ -191,18 +177,9 @@ FROM
 	FROM Phong p
 	) q
 WHERE q.MaPh = ThuePhong.MaPh 
-	
-– 
+	 
 UPDATE HoaDon
 SET TongTien = tp.ThanhTien
 FROM ThuePhong tp
 WHERE tp.MaTP = HoaDon.MaTP
-
-
-
-
-
-
-
-
 
