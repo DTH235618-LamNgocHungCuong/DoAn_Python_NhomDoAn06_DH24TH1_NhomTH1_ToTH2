e0d47b649414e38f434403d45ee031d0cf0c79ed
DTH235618-LamNgocHungCuong <cuong_dth235618@student.agu.edu.vn>
2025-12-02 16:13:43 +0700

    Update KhachHang.py

--- KhachHang.py
+++ KhachHang.py
@@ -2,8 +2,9 @@ from tkinter import *
 from tkinter import ttk, messagebox
 #from tkcalendar import DateEntry
 from datetime import datetime, date
-from SQL_connec import conn, cur
+#from SQL_connec import conn, cur
 from tkinter import filedialog, messagebox
+from Menu import mo_menu
 
 def open_KhachHang():
    def center_window(win, w=900, h=700):
@@ -47,7 +48,7 @@ def open_KhachHang():
    frame_bang.pack(pady=5, expand=True)
 
    # Treeview
-   columns = ("Mã khách hàng", "Tên khách hàng", "Điện thoại", "Địa chỉ")  # cần dấu phẩy ở cuối
+   columns = ("Mã_khách_hàng", "Tên_khách_hàng", "Điện_thoại", "Địa_chỉ")  # cần dấu phẩy ở cuối
    tree = ttk.Treeview(frame_bang, columns=columns, show="headings", height=10)
 
    # Thanh cuộn
@@ -58,9 +59,16 @@ def open_KhachHang():
    scroll_y.pack(side="right", fill="y")
    scroll_x.pack(side="bottom", fill="x")
    tree.pack(side="left", expand=True)
-   for col in columns:
-       tree.heading(col, text=col.title())
-       tree.column(col, width=150, anchor="center")
+
+   tree.heading("Mã_khách_hàng", text="Mã_khách_hàng")
+   tree.heading("Tên_khách_hàng", text="Tên_khách_hàng")
+   tree.heading("Điện_thoại", text="Điện_thoại")
+   tree.heading("Địa_chỉ", text="Địa_chỉ")
+   
+   tree.column("Mã_khách_hàng", width=60, anchor="center")
+   tree.column("Tên_khách_hàng", width=150)
+   tree.column("Điện_thoại", width=150)
+   tree.column("Địa_chỉ", width=150)
 
    tree.pack(padx=5, pady=10, fill="x")
 
@@ -93,17 +101,10 @@ def open_KhachHang():
        except Exception as e:
            messagebox.showerror("Lỗi", f"Lỗi load dữ liệu{e}")
        
+       
    def suaten(tenkh):
-       s = tenkh
-       s = s.strip()
-       arr = s.split(' ')
-       s = ""
-       for i in arr:
-           word = i
-           if len(word.strip()) != 0:
-               s += word + "_"
-       return s.strip()
-
+       parts = tenkh.strip().split()
+       return '_'.join(parts)
 
    def them_KH():
        makh = entry_mkh.get()
@@ -119,14 +120,14 @@ def open_KhachHang():
            messagebox.showerror("Lỗi","MaKH không hợp lệ")
            return
 
-
+       
        try:
            makh = suaten(makh)
            # kt xem tang có bị trùng ko
            cur.execute("SELECT COUNT(*) FROM KhachHang where MaKH = %s", (makh,))
 
            if cur.fetchone()[0] > 0:
-               messagebox.showwarning("Trùng lập", f"Phòng {makh} đã tồn tại")
+               messagebox.showwarning("Trùng lập", f"Khách hành {makh} đã tồn tại")
                return
 
            cur.execute("Insert into KhachHang (MaKH, TenKH, DThoaiKH, DChiKH) VALUES (%s, %s, %s, %s)", (makh, tenkh,dthoai,dchi))
@@ -136,6 +137,7 @@ def open_KhachHang():
            messagebox.showinfo("Thành công", "Đã thêm phòng mới")
        except Exception as e:
            messagebox.showerror("Lỗi", f"{e}")
+       
 
    def xoa_KH():
        selected = tree.selection()
@@ -154,6 +156,7 @@ def open_KhachHang():
            messagebox.showinfo("Đã xoá")
        except Exception as e:
            messagebox.showerror("Lỗi", f"Lỗi khi xoá:\n{e}")
+       
 
    def sua_KH():
        selected = tree.selection()
@@ -165,7 +168,7 @@ def open_KhachHang():
        entry_mkh.insert(0, values[0]) #khóa
        entry_mkh.config(state='disabled')
        entry_tkh.delete(0, END)
-       entry_tkh.config(0, values[1])
+       entry_tkh.insert(0, values[1])
        entry_dt.delete(0, END)
        entry_dt.insert(0, values[2])
        entry_dc.delete(0, END)
@@ -189,17 +192,20 @@ def open_KhachHang():
        tenkh = entry_tkh.get()
        dthoai = entry_dt.get()
        dchi = entry_dc.get()
-
-       makh = suaten(makh)
-      
+       
        try:
-           cur.execute("""UPDATE KhachHang SET MaKH=%s, TenKH=%s, DThoaiKH=%s, DChiKH=%s""", (makh,tenkh,dthoai,dchi))
+           cur.execute("""UPDATE KhachHang SET TenKH=%s, DThoaiKH=%s, DChiKH=%s where MaKH=%s""", (tenkh,dthoai,dchi,makh))
            conn.commit()
            load_data()
            clear_input()
            messagebox.showinfo("Thành công", "Cập nhật thành công.")
        except Exception as e:
            messagebox.showerror("Lỗi", f"Lỗi khi lưu:\n{e}")
+
+   def thoat_KH():
+       rootKH.destroy()
+       mo_menu()
+
 
    frame_btn = Frame(rootKH)
    frame_btn.pack(padx=5, pady=5, anchor="center")
@@ -208,7 +214,7 @@ def open_KhachHang():
    Button(frame_btn, text="Lưu", width=8, command=luu_KH).grid(row=0, column=1, padx=5)
    Button(frame_btn, text="Sửa", width=8, command=sua_KH).grid(row=1, column=0, padx=5)
    Button(frame_btn, text="Xoá", width=8, command=xoa_KH).grid(row=1, column=1, padx=5)
-   Button(frame_btn, text="Thoát", width=8, command=rootKH.quit).grid(row=0, column=2, padx=5)
+   Button(frame_btn, text="Thoát", width=8, command=thoat_KH).grid(row=0, column=2, padx=5)
 
    
 
@@ -216,3 +222,4 @@ def open_KhachHang():
 
 
    rootKH.mainloop()
+
